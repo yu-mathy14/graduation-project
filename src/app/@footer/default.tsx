8a4d8a436160d968fad4df78b678805e51051704
yu-mathy14 <yuna14.navi1031@gmail.com>
2026-09-07 09:04:22 +0000

    260907終：仮トップページの作成

---
+++ src/app/@footer/default.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2026 basketball manager</p>
+    </footer>
+  );
+}
---
+++ src/app/＠header/default.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>🏀Basketball Manager🏀</h1>
+    </header>
+  );
+}
